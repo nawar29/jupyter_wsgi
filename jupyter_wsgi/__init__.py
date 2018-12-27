bf6b2cf2fcdd8d43f3565e0a2d4cc54e8cd070f9
Isaac Henry <isaac.henry@soterawireless.com>
2018-12-27 19:47:11 +0000

    Initial commit

---
+++ jupyter_wsgi/__init__.py
@@ -0,0 +1,31 @@
+import asyncio
+import getpass
+from aiohttp import web
+from aiohttp_wsgi import WSGIHandler
+from aiohttp.web import middleware
+from aiohttp.web_runner import TCPSite
+from IPython import display
+
+class AppViewer(object):
+
+    def __init__(self, base_url, port=8050):
+        self.web_app = web.Application()
+        self.port = port
+        self.requests_pathname_prefix = '/user/{}/proxy/{}/'.format(getpass.getuser(), port)
+        self.url = '{}/{}'.format(base_url, self.requests_pathname_prefix)
+        
+    async def show(self, wsgi_app, width=300, height=300):
+        self.wsgi_handler = WSGIHandler(wsgi_app)
+        self.resource = self.web_app.router.add_route("*", "/{path_info:.*}", self.wsgi_handler)
+        self.runner = web.AppRunner(self.web_app)
+        await self.runner.setup()
+        self.site = TCPSite(self.runner, port=self.port)
+        await self.site.start()
+        display.display(display.IFrame(self.url, width, height))
+
+    async def stop(self):
+        await self.site.stop()
+
+    async def terminate(self):
+        await self.site.stop()
+        
---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+
+
+NAME = 'jupyter_wsgi'
+DESCRIPTION = 'Run a wsgi app in a Jupyter Notebook'
+URL = 'https://github.com/ihenry42/jupyter_wsgi'
+EMAIL = 'ihenry42@gmail.com'
+AUTHOR = 'Isaac Henry'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = '0.1.0'
+
+REQUIRED = [
+    'aiohttp', 'aiohttp_wsgi'
+]
+
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    install_requires=REQUIRED,
+    license='MIT')
