e8d08e0c3cbb3e069c25ffbbb32339939cf5815b
= <=>
2019-01-11 22:38:02 +0000

    fixed bugs from last commit

--- jupyter_wsgi/__init__.py
+++ jupyter_wsgi/__init__.py
@@ -8,7 +8,7 @@ from IPython import display
 class AppViewer(object):
 
     _exception_ = None
-    
+
     def __init__(self, base_url, port=8050):
         self.web_app = web.Application()
         self.port = port
@@ -16,26 +16,29 @@ class AppViewer(object):
         self.url = '{}/{}'.format(base_url, self.requests_pathname_prefix)
         self.site = None
         self._running = False
-        
-    async def show(self, wsgi_app, width=300, height=300):
-        if self.site is None:
+
+    async def setup(self, wsgi_app):
+         if self.site is None:
             self.wsgi_handler = WSGIHandler(wsgi_app)
             self.resource = self.web_app.router.add_route("*", "/{path_info:.*}", self.wsgi_handler)
             self.runner = web.AppRunner(self.web_app)
             await self.runner.setup()
-            self.site = TCPSite(self.runner, port=self.port)            
+            self.site = TCPSite(self.runner, port=self.port)
+
+    async def show(self, wsgi_app, width=300, height=300):
+        await self.setup(wsgi_app)
         if not self._running:            
             await self.site.start()
             self._running = True
-
         display.display(display.IFrame(self.url, width, height))
-            
+
     async def stop(self):
         await self.site.stop()
+        self._running = False
 
     async def terminate(self):
-        await self.site.stop()
-        
+        await self.stop()
+
     def handle_exceptions(self, the_type):
         def notenook_decorate(func):
             def wrapper(arg):
