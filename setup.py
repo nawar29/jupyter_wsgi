38686b2631ef22f99af7ea44e1a0b3ae3bcc6594
= <=>
2019-01-21 23:58:42 +0000

    added find_packages to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 NAME = 'jupyter_wsgi'
@@ -23,6 +23,7 @@ else:
 
 setup(
     name=NAME,
+    packages = find_packages(),
     version=about['__version__'],
     description=DESCRIPTION,
     author=AUTHOR,
